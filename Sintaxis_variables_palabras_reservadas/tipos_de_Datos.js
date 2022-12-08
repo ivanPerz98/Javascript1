d6ea4f3005a5adb4aba0390b37b7b5d19dc31a53
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-08 17:37:35 -0300

    QUE ES NULL E UNDEFINED

--- Sintaxis_variables_palabras_reservadas/tipos_de_Datos.js
+++ Sintaxis_variables_palabras_reservadas/tipos_de_Datos.js
@@ -2,11 +2,17 @@
 
 //number
 var a = 5050
+console.log(typeof(a))
 
 //strings
 var strings = "Esto es un Strings"
-
+console.log(typeof(strings))
 //boolean
 var bool = true // o false
+console.log(typeof(bool))
 
-console.log(a)
+
+//null e undefined
+
+//NULL ==> TIENE LUGAR EN MEMORIA PERO ESTA VACIO
+//UNDEFINED ==> NO TIENE LUGAR EN MEMORIA ,TODAVIA NO SE LE ASIGNO
