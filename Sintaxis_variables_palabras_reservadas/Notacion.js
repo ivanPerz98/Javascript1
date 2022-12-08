d2039b4fcef328d87520555d07fd18feb6e311a1
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-08 18:33:46 -0300

    notacion de los distintos tipo de datos que existen

---
+++ Sintaxis_variables_palabras_reservadas/Notacion.js
@@ -0,0 +1,24 @@
+// Notacion
+
+// ; --> delimitar el final de una linea, el ponerlo es opcional
+
+const b = 4;
+
+// . --> se utiliza para acceder a atributos de objetos
+
+//[] --> listas,arrays
+
+//() --> funciones
+
+function suma(a,b){
+    //funcion a escribir
+}
+
+//{} --> objetos
+
+const movil = {
+    ancho:5,
+    altura:10
+}
+
+console.log(movil.ancho)
