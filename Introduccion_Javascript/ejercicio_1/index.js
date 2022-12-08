ce408082e4ec9f820721d5396e44695239231b12
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-08 16:51:53 -0300

    ejercicio 1 terminado

---
+++ Introduccion_Javascript/ejercicio_1/index.js
@@ -0,0 +1,4 @@
+/* crea un script que muestre por pantalla "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".*/ 
+
+console.log( "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+
