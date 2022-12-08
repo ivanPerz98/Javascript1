529c32aec2435e5cd896de9f903189f35f17a873
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-08 20:10:08 -0300

    Realizado el ejercicio numero 2 de Sintaxis_variables_palabras_reservadas

---
+++ Sintaxis_variables_palabras_reservadas/index.js
@@ -0,0 +1,22 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/ 
+
+
+const lista = [
+    "Ivan",
+    24,
+    false,
+    "4/4/1998",
+    Favourite_Book={
+        'titulo':'1984',
+        'autor':'George Orwell',
+        'Fecha': '8/6/1949',
+        'url_amazon':'https://www.amazon.com/-/es/George-Orwell/dp/8499890946'
+    }
+]
