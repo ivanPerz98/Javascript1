9d2e18d6ed22b9e437496c231c2827e7f515105c
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-09 21:25:54 -0300

    Expresione regular y mas metodos

---
+++ cadenas_de_texto/expresiones_regulares_y_mas_metodos.js
@@ -0,0 +1,16 @@
+let texto = "Pedro Pablo Pérez Pereira, pobre pintor portugués, pinta preciosos paisajes por poca plata, para poder pasar por París."
+
+
+//DEVUELVE EN FORMA DE LISTA LA CANTIDAD DE VECES QUE UNA PALBRA O LETRA ESTA DENTRO DE OTRA
+console.log(texto.match(/n/g)) // ['n','n'] 
+
+console.log(texto.includes("Pablo"))  //true --> verifica si un texto esta dentro de otra y devuelve un booleano
+
+
+//Saber si empieza con una palabra 
+
+console.log(texto.startsWith('Pedro')) //TRUE
+
+//Saber si termin con una palabra
+
+console.log(texto.endsWith("paris"))  //FALSE, no es lo mismo p y P
