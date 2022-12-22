c99ba730177b6a5f68607e7b63d53bcc3db3c63b
Ivan Perez <perez.ivan9804@gmail.com>
2022-12-21 23:44:59 -0300

    modificar para que parezca python

--- Modulos/EjercicioModulos/index.js
+++ Modulos/EjercicioModulos/index.js
@@ -1,10 +1,10 @@
 import { suma,multiplica } from "./controllers.js";
 import chalk from "chalk";
 
-const log = console.log;
+const print = console.log;
 
 const suma1 = suma(1,2)
 const suma2 = suma(4,5)
 
-log(chalk.green(( `El resultado es: ${multiplica(suma1,suma2)} `)));
+print(chalk.green(( `El resultado es: ${multiplica(suma1,suma2)} `)));
 
